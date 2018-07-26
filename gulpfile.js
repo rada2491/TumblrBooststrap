227f69ac08950dfe0be226cea0a33352cad15fcb
Carlos <cbr2491@gmail.com>
2018-07-26 16:59:09 -0600

    wip

--- gulpfile.js
+++ gulpfile.js
@@ -23,19 +23,28 @@ gulp.task('styles', () => {
   return gulp.src('src/sass/main.scss')
     .pipe(sass({includePaths: [
       path.join(__dirname, 'node_modules/bootstrap-sass/assets/stylesheets'),
-      path.join(__dirname, 'src/sass')]
+      path.join(__dirname, 'src/sass'),
+      path.join(__dirname, 'node_modules/font-awesome')]
       , outputStyle: 'compressed'}))
     .pipe(gulp.dest('dist/css/'))
      gulp.start('lint');
 })
 
+/* HTML task */
 gulp.task('html', () => {
   return gulp.src('src/**/*.html')
     .pipe(gulp.dest('dist/'))
 })
 
+/* Assets task */
 gulp.task('assets', () => {
   return gulp.src('assets/*.png').pipe(gulp.dest('dist/img/'))
+});
+
+/* Font task */
+
+gulp.task('font-awesome', () => {
+  return gulp.src('node_modules/font-awesome/fonts/*').pipe(gulp.dest('dist/fonts/'))
 });
 
 gulp.task('watch', () => {
@@ -55,6 +64,7 @@ gulp.task('server', () => {
 gulp.task('build', [
   'html',
   'styles',
+  'font-awesome',
   'assets',
   'server',
   'watch'
